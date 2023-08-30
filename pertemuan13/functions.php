fe3068e454cd305277e13d6fc4d3683f5f359523
akhmadfaizabdulloh <akhmadfaizabdulloh@gmail.com>
2023-08-30 16:45:06 +0700

    upload file for tambah.php (pertemuan13) finish

--- pertemuan13/functions.php
+++ pertemuan13/functions.php
@@ -43,8 +43,81 @@ function tambah($data) {
 
 
 function upload() {
-    return false;
+    
+    $namaFile = $_FILES['gambar']['name'];
+    $ukuranFile = $_FILES['gambar']['size'];
+    $error = $_FILES['gambar']['error'];
+    $tmpName = $_FILES['gambar']['tmp_name'];
+
+    
+    // cek apakah tidak ada gambar yang diupload
+    if ( $error === 4 ) {
+        echo "<script>
+                alert('pilih gambar terlebih dahulu!');
+              </script>";
+        
+        return false;
+    }
+
+
+    // cek apakah yang diupload adalah gambar
+    $eksensiGambarValid = ['jpg', 'jpeg', 'png'];
+
+    // explode(delimiter, string)
+    $eksensiGambar = explode('.', $namaFile);
+    // sandhika.jpg = ['sandhika', 'jpg']
+
+    // jika kita pakai ini
+    // $eksensiGambar = $eksensiGambar[1]
+    // jika nama file-nya sandika.galih.jpg
+    // maka yang di ambil "galih"
+
+    $eksensiGambar = strtolower(end($eksensiGambar));
+    // end() >>> untuk mengambil yang terakhir
+    // srttolower >>> untuk mengatasi jika ada nama file "sandhika.JPG" agar di convert menjadi huruf kecil (jpg).
+
+    if( !in_array($eksensiGambar, $eksensiGambarValid) ) {
+        echo "<script>
+                alert('yang anda upload bukan gambar!');
+              </script>";
+        return false;
+    }
+
+    // in_array(needle, haystack)
+    // in_array(jarum, jerami)
+
+
+
+    // cek jika ukurannya terlalu besar
+    if( $ukuranFile > 100000 ){
+        echo "<script>
+                alert('ukuran gambar terlalu besar!');
+              </script>";
+        return false;
+    }
+    // 100.000 byte = 100 kb
+    // 1.000.000 byte = 1 MB
+
+
+    // lolos pengecekan, gambar siap diupload
+    // generte nama gambar baru
+
+    $namaFileBaru = uniqid();
+    $namaFileBaru .= '.';
+    $namaFileBaru .= $eksensiGambar;
+
+    // var_dump($namaFileBaru); die;
+
+
+    move_uploaded_file($tmpName, 'img/' . $namaFileBaru);
+
+    // move_uploaded_file(filename, destination)
+
+    return $namaFileBaru;
+    // untuk $gambar
+
 }
+
 
 function hapus($id) {
     global $conn;
