7223e810a76c1e505075b32b0b2c9b49bf725b58
akhmadfaizabdulloh <akhmadfaizabdulloh@gmail.com>
2023-09-01 23:38:25 +0700

    pertemuan15 - login finish

--- pertemuan15/login.php
+++ pertemuan15/login.php
@@ -1,3 +1,30 @@
+<?php
+require 'functions.php';
+
+if( isset($_POST["login"]) ) {
+
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    $result = mysqli_query($conn, "SELECT * FROM user WHERE username = '$username'");
+
+    // cek username
+    if( mysqli_num_rows($result) === 1 ) {
+
+        // cek password
+        $row = mysqli_fetch_assoc($result);
+        if ( password_verify($password, $row["password"]) ) {
+            header("Location: index.php");
+            exit;
+        }
+    }
+
+    $error = true;
+
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,6 +35,10 @@
 <body>
     
     <h1>Halaman Login</h1>
+
+    <?php if( isset($error) ) : ?>
+        <p style="color: red; font-style: italic;">username / password salah!</p>
+        <?php endif; ?>
 
     <form action="" method="post">
         <ul>
