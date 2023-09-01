bf40e9099d7108b0741b2749660417dae2591476
akhmadfaizabdulloh <akhmadfaizabdulloh@gmail.com>
2023-09-01 20:46:51 +0700

    copy from pertemuan14

---
+++ pertemuan15/tambah2.php
@@ -0,0 +1,80 @@
+<!-- tambahkan atribut html 5 "required" pada form input jika ada data yang tidak boleh kosong -->
+
+<!-- bagaimana jika user mengisikan element html seperti <h1>sandikgalih@gmail.com</h1> -->
+
+<?php
+// koneksi ke DBMS
+$conn = mysqli_connect("localhost", "root", "root", "phpdasar");
+
+// cek apakah tombol submit sudah ditekan atau belum
+if( isset($_POST["submit"]) ) {
+
+    // ambil data dari tiap elemen dalam form
+    $nama = $_POST["nama"];
+    $nrp = $_POST["nrp"];
+    $email = $_POST["email"];
+    $jurusan = $_POST["jurusan"];
+    $gambar = $_POST["gambar"];
+
+    // query insert data
+    $query = "INSERT INTO mahasiswa
+                VALUES
+                (0, '$nama','$nrp','$email','$jurusan','$gambar')";
+
+    mysqli_query($conn, $query);
+
+    // cek apakah data berhasil di tambahkan atau tidak
+    // var_dump(mysqli_affected_rows($conn));
+
+    if(mysqli_affected_rows($conn) > 0) {
+        echo "berhasil";
+    } else {
+        echo "gagal";
+        echo "<br>";
+        echo mysqli_error($conn);
+    }
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tambah data mahasiswa</title>
+</head>
+<body>
+    <h1>Tambah data mahasiswa</h1>
+
+    <form action="" method="post">
+        <ul>
+            <li>
+                <label for="nrp">NRP : </label>
+                <input type="text" name="nrp" id="nrp">
+            </li>
+            <li>
+                <label for="nama">Nama : </label>
+                <input type="text" name="nama" id="nama">
+            </li>
+            <li>
+                <label for="email">Email : </label>
+                <input type="text" name="email" id="email">
+            </li>
+            <li>
+                <label for="jurusan">Jurusan : </label>
+                <input type="text" name="jurusan" id="jurusan">
+            </li>
+            <li>
+                <label for="gambar">Gambar : </label>
+                <input type="text" name="gambar" id="gambar">
+            </li>
+            <li>
+                <button type="submit" name="submit">Tambah Data!</button>
+            </li>
+        </ul>
+    </form>
+    
+</body>
+</html>
