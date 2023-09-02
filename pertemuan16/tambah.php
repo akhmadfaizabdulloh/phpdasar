e626af5a1dd272df4354a9778fd1a80c7b64915e
akhmadfaizabdulloh <akhmadfaizabdulloh@gmail.com>
2023-09-02 15:10:06 +0700

    duplicat pertemuan15

---
+++ pertemuan16/tambah.php
@@ -0,0 +1,75 @@
+<?php
+require 'functions.php';
+
+// cek apakah tombol submit sudah ditekan atau belum
+if( isset($_POST["submit"]) ) {
+
+    // debugging (melihat isi dari $_POST)
+    // var_dump($_POST);
+    // var_dump($_FILES);
+    // // die; agar program setelahnya tidak di jalankan
+    // die; 
+
+
+    // cek apakah data berhasil di tambahkan atau tidak
+    if( tambah($_POST) > 0 ) {
+        echo "
+            <script>
+            alert('data berasil ditambahkan!');
+            document.location.href = 'index.php';
+            </script>
+        ";
+    } else {
+        echo "
+            <script>
+            alert('data gagal ditambahkan!');
+            document.location.href = 'index.php';
+            </script>
+        ";
+    }
+    
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tambah data mahasiswa</title>
+</head>
+<body>
+    <h1>Tambah data mahasiswa</h1>
+
+    <form action="" method="post" enctype="multipart/form-data">
+        <ul>
+            <li>
+                <label for="nrp">NRP : </label>
+                <input type="text" name="nrp" id="nrp" required>
+            </li>
+            <li>
+                <label for="nama">Nama : </label>
+                <input type="text" name="nama" id="nama">
+            </li>
+            <li>
+                <label for="email">Email : </label>
+                <input type="text" name="email" id="email">
+            </li>
+            <li>
+                <label for="jurusan">Jurusan : </label>
+                <input type="text" name="jurusan" id="jurusan">
+            </li>
+            <li>
+                <label for="gambar">Gambar : </label>
+                <input type="file" name="gambar" id="gambar">
+            </li>
+            <li>
+                <button type="submit" name="submit">Tambah Data!</button>
+            </li>
+        </ul>
+    </form>
+    
+</body>
+</html>
