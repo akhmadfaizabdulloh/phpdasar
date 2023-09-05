261c8eb3e2aacb6411802bd34cce908c9a7b78ef
akhmadfaizabdulloh <akhmadfaizabdulloh@gmail.com>
2023-09-05 19:51:24 +0700

    pertemuan17 - menghapus cookie pada logout

--- pertemuan17/logout.php
+++ pertemuan17/logout.php
@@ -6,6 +6,13 @@ $_SESSION = [];
 session_unset();
 session_destroy();
 
+// menghapus cookie
+setcookie('id', '', time() - 3600);
+setcookie('key', '', time() - 3600);
+// mundurkan 3600 (1jam) mengikuti dokumentasi php.net
+
+
+
 header("Location: login.php");
 exit;
 
